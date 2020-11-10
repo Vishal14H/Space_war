de1188475baecee86f40cfd7fb217cd3dc5857c6
Vishal singh <44376633+Vishal14H@users.noreply.github.com>
2020-11-11 01:31:54 +0530

    Adding Damage Effect To Enemy

--- Space War/Assets/Scripts/Player.cs
+++ Space War/Assets/Scripts/Player.cs
@@ -72,6 +72,7 @@ public class Player : MonoBehaviour
 
     private void Die() 
     {
+        FindObjectOfType<Level>().LoadGameOver();
         Destroy(gameObject);
         AudioSource.PlayClipAtPoint(deathSFX, Camera.main.transform.position, deathSFXVolume);
     }
